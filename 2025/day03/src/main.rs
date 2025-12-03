3a55f43b222571a2c19d37b72be91ef723493202
Bruno de Castro Brezolin <bruno@geniusws.com.br>
2025-12-03 17:53:59 -0300

    day 03 part 1

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -1,29 +1,47 @@
-use common::expect;
-use std::{fs::read_to_string, path::Path};
+use common::{expect, read_input};
 
-fn part1(_input: String) -> i32 {
+fn part1(input: Vec<Vec<u32>>) -> u32 {
+    let mut sum = 0;
+
+    input.iter().for_each(|line| {
+        if line.len() < 2 {
+            panic!("Expected all lines to have at least 2 digits");
+        }
+
+        let mut it = line.iter().rev();
+        let mut second_largest = it.next().unwrap().clone();
+        let mut largest = it.next().unwrap().clone();
+
+        it.for_each(|digit| {
+            if *digit >= largest {
+                if largest > second_largest {
+                    second_largest = largest.clone();
+                }
+                largest = digit.clone();
+            }
+        });
+
+        sum += largest * 10 + second_largest;
+    });
+
+    sum
+}
+
+fn part2(_input: Vec<Vec<u32>>) -> u64 {
     0
 }
 
-fn part2(_input: String) -> i32 {
-    0
-}
-
-fn get_input(file_path: &str) -> Option<String> {
-    let path = Path::new(file_path);
-    if !path.exists() {
-        return None;
-    }
-    Some(read_to_string(path).expect("File can't be read"))
+fn parse_line(line: &str) -> Vec<u32> {
+    line.trim().chars().filter_map(|d| d.to_digit(10)).collect()
 }
 
 fn main() {
-    let example_input = get_input("inputs/day03/example.txt");
-    let real_input = get_input("inputs/day03/real.txt");
+    let example_input = read_input("inputs/day03/example.txt", parse_line);
+    let real_input = read_input("inputs/day03/real.txt", parse_line);
 
     println!("Part 1");
     if let Some(input) = &example_input {
-        expect(part1(input.clone()), None);
+        expect(part1(input.clone()), Some(357));
     }
     if let Some(input) = &real_input {
         expect(part1(input.clone()), None);
@@ -31,7 +49,7 @@ fn main() {
 
     println!("Part 2");
     if let Some(input) = &example_input {
-        expect(part2(input.clone()), None);
+        expect(part2(input.clone()), Some(3121910778619));
     }
     if let Some(input) = &real_input {
         expect(part2(input.clone()), None);
